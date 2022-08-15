67f33a9ba290e6807b9f4de97885962f52890a6a
wmc <wengmaochun@bnqkl.cn>
2022-08-15 17:37:26 +0800

    :bug: 编译

--- packages/@types/bfchain__pc-sdk-api/network/websocketHelper.d.ts
+++ packages/@types/bfchain__pc-sdk-api/network/websocketHelper.d.ts
@@ -2,7 +2,7 @@
 import type { ApiConfigHelper } from "@bfchain/pc-sdk-helper-api-config";
 import { REQUEST_PROTOCOL } from "@bfchain/pc-sdk-api-constants";
 export declare class WebsocketHelper {
-    private __socket;
+    private __socketMap;
     private __transactionServerPort;
     private __configHelper;
     private __config;
@@ -11,9 +11,7 @@ export declare class WebsocketHelper {
     readonly TRANSACTION_SERVER_URL_PREFIX: string;
     constructor(transactionServerPort: number, configHelper: ApiConfigHelper);
     private __getUrl;
-    get URL(): string;
     private __getWebsocketHost;
-    get WEBSOCKET_HOST(): string;
     createTransaction<T>(url: string, argv: {
         [key: string]: any;
     }): Promise<T>;
